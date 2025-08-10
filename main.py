1c4fa8e93b50f958a8e8fa95f711485aa9a2d26d
thelostone9 <jrussell11702@gmail.com>
2025-08-10 17:38:18 -0400

    added circleshape.py

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -12,6 +14,8 @@ def main():
 
         screen.fill("black")
         pygame.display.flip()
+        clock.tick(60)
+        dt = clock.tick(60) / 1000
 
 
 
